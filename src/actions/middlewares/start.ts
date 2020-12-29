e5d60a4a74537314eec542856d5da50f66916fa5
Geno Ferollo <genemators@gmail.com>
2020-12-29 23:32:26 +0500

    switch case typo

--- src/actions/middlewares/start.ts
+++ src/actions/middlewares/start.ts
@@ -2,13 +2,23 @@ import { composer, middleware } from '@core/bot'
 import * as consoles from '@layouts/consoles'
 import * as message from '@layouts/messages'
 import * as keyboard from '@layouts/keyboards'
-import { TelegrafContext } from 'telegraf/typings/context'
+import { TelegrafContext } from '@type/telegraf'
 
 composer.start(async (ctx: TelegrafContext) => {
-    await ctx.replyWithHTML(message.start, {
-        parse_mode: 'HTML',
-        reply_markup: keyboard.start
-    })
+    switch (ctx.startPayload) {
+        case 'intranet':
+            await ctx.replyWithHTML(`Intranet message here`, {
+                parse_mode: 'HTML',
+                reply_markup: keyboard.start
+            })
+            break
+        default:
+            await ctx.replyWithHTML(message.start, {
+                parse_mode: 'HTML',
+                reply_markup: keyboard.start
+            })
+            break
+    }
 })
 
 middleware(composer)
--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,6 @@
 import 'module-alias/register'
 import '@src/core/index'
 import '@actions/index'
+
+// TODO LISTS!!!
+// TODO: ADD TIMETABLE UPDATER AT THE NIGHT TIME OF THE DAY
