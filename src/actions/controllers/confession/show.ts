f78d3ccbfd8876f25b08f583e25146bbd63aae98
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-18 04:12:05 +0500

    Update show.ts

--- src/actions/controllers/confession/show.ts
+++ src/actions/controllers/confession/show.ts
@@ -1,13 +1,16 @@
 import { TelegrafContext } from "@type/telegraf";
 import { scheme } from "@database/user";
 import * as message from "@layouts/messages";
+import cursed from "@actions/controllers/confession/cursed";
+import encoder from "@actions/controllers/confession/encoder";
 
 export default async function (ctx: TelegrafContext): Promise<void> {
   const templating = async (): Promise<string> => {
+    const cleared = await cursed(scheme[ctx.from.id].messages.join("\n"));
     return (
-      `<b>🔰 Announcement</b> \n` +
+      `<b>🔰 #${await encoder(ctx.from.id)}</b> \n` +
       `\n` +
-      `${scheme[ctx.from.id].messages.join("\n")} \n` +
+      `${cleared} \n` +
       `\n` +
       `<b>Sincerely, Westmaid!</b> \n`
     );
