95dcdc2639e902edffd0ff60b924fc39d0763a2e
UwUssimo Robinson <uwussimo@icloud.com>
2022-01-27 06:44:32 +0500

    cron fixed up

--- src/delta/cron/resource.ts
+++ src/delta/cron/resource.ts
@@ -5,16 +5,24 @@ export const message = (subject) =>
   `\n` +
   `<b>10 minutes left</b> for <code>${subject.name} ${subject.type}</code> class. ` +
   `Please, get ready as soon as possible!` +
-  `\n` +
-  `\n` +
-  `You should attend the seminars on-campus and lectures online.` +
+  // `\n` +
+  // `\n` +
+  // `You should attend the seminars on-campus and lectures online.` +
   `\n` +
   `\n` +
   `The class is going to be held by <b>${subject.tutor}</b> ${
     subject.type === "online"
       ? "on the internet <b>online</b>"
-      : `at <code>${subject.location}</code>`
+      : 'on intranet. Press the "Open the intranet" button below to get to <b>LMS</b> quickly and get ready to classes!' // `at <code>${subject.location}</code>`
   } \n`;
 
 export const keyboard = (link) =>
-  Markup.inlineKeyboard([[Markup.urlButton("Check out the timetable", link)]]);
+  Markup.inlineKeyboard([
+    [Markup.urlButton("Check out the timetable", link)],
+    [
+      Markup.urlButton(
+        "Open the intranet",
+        `https://intranet.wiut.uz/UserModuleMaterials`
+      ),
+    ],
+  ]);
