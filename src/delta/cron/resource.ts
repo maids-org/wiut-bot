5b90fa76ce95cb6f02177bd58d34692022380d60
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-11 11:29:13 +0500

    1 hour before class

--- src/delta/cron/resource.ts
+++ src/delta/cron/resource.ts
@@ -1,28 +1,44 @@
 import { Markup } from "telegraf";
 
-export const message = (subject, group) =>
-  `<b>⚠️ Upcoming Class Notification for ${group}</b> \n` +
-  `\n` +
-  `<b>10 minutes left</b> for <code>${subject.name} ${subject.type}</code> class. ` +
-  `Please, get ready as soon as possible!` +
-  `\n` +
-  `\n` +
-  `You should attend the seminars on-campus or online.` +
-  `\n` +
-  `\n` +
-  `The class is going to be held by <b>${subject.tutor}</b> ${
-    subject.type === "online"
-      ? "on the internet <b>online</b>"
-      : `at <code>${subject.location}</code>`
-  } \n`;
+export const message = (isTenMinuteLeft: boolean, subject, group) =>
+  isTenMinuteLeft
+    ? `<b>⚠️ Upcoming Class Notification for ${group}</b> \n` +
+      `\n` +
+      `<b>10 minutes left</b> for <code>${subject.name} ${subject.type}</code> class. ` +
+      `Please, get ready as soon as possible!` +
+      `\n` +
+      `\n` +
+      `It's up to you, whether you will you attend classes online on intranet or offline on campus. However, administration want you to attend classes offline.` +
+      `\n` +
+      `\n` +
+      `The class is going to be held by <b>${subject.tutor}</b> ${
+        subject.type === "online"
+          ? "on the internet <b>online</b>"
+          : `at <code>${subject.location}</code>`
+      } \n`
+    : `<b>⚠️ Upcoming Class Notification for ${group}</b> \n` +
+      `\n` +
+      `<b>An hour left</b> for <code>${subject.name} ${subject.type}</code> class. ` +
+      `If you are studying offline, better you go now! There might be traffic jam or some events going on...` +
+      `\n` +
+      `\n` +
+      `The class is going to be held by <b>${subject.tutor}</b> ${
+        subject.type === "online"
+          ? "on the internet <b>online</b>"
+          : `at <code>${subject.location}</code>`
+      } \n`;
 
-export const keyboard = (link) =>
-  Markup.inlineKeyboard([
-    [Markup.urlButton("Check out the timetable", link)],
-    [
-      Markup.urlButton(
-        "Open the intranet for online",
-        `https://intranet.wiut.uz/UserModuleMaterials`
-      ),
-    ],
-  ]);
+export const keyboard = (isTenMinuteLeft: boolean, link: string) =>
+  isTenMinuteLeft
+    ? Markup.inlineKeyboard([
+        [Markup.urlButton("Check out the timetable", link)],
+        [
+          Markup.urlButton(
+            "Open the intranet for online",
+            `https://intranet.wiut.uz/UserModuleMaterials`
+          ),
+        ],
+      ])
+    : Markup.inlineKeyboard([
+        [Markup.urlButton("Check out the timetable", link)],
+      ]);
--- src/delta/cron/timetable.ts
+++ src/delta/cron/timetable.ts
@@ -14,15 +14,42 @@ function sleep(ms) {
     const timetable = new Timetable(group);
     for (const day of Object.keys(timetable.getAllLessons())) {
       for (const subject of timetable.getAllLessons()[day]) {
+        // 1 hour before
+        cron.schedule(
+          `00 ${subject.start - 1.0} * * ${day}`,
+          async () => {
+            await bot.telegram
+              .sendMessage(
+                (await dungeon.getByMod(group)).id,
+                resource.message(false, subject, group),
+                {
+                  reply_markup: resource.keyboard(
+                    false,
+                    timetable.getTimetableLink()
+                  ),
+                  parse_mode: "HTML",
+                }
+              )
+              .catch(null);
+          },
+          {
+            timezone: "Asia/Tashkent",
+          }
+        );
+
+        // 10 minutes before
         cron.schedule(
           `50 ${subject.start - 1.0} * * ${day}`,
           async () => {
             await bot.telegram
               .sendMessage(
                 (await dungeon.getByMod(group)).id,
-                resource.message(subject, group),
+                resource.message(true, subject, group),
                 {
-                  reply_markup: resource.keyboard(timetable.getTimetableLink()),
+                  reply_markup: resource.keyboard(
+                    true,
+                    timetable.getTimetableLink()
+                  ),
                   parse_mode: "HTML",
                 }
               )
