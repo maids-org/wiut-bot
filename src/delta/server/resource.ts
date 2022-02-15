7f45ba80ba487c14d2ebd05f1672167adf5b49a6
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-15 18:51:29 +0500

    server command fixed

--- src/delta/server/resource.ts
+++ src/delta/server/resource.ts
@@ -1,14 +1,9 @@
 import { Markup } from "telegraf";
-import { TelegrafContext } from "@type/telegraf";
 
-export const message = (mod: string, ctx: TelegrafContext, isAdmin: boolean) =>
-  `<b>👤 User:</b> ${ctx.from.first_name}` +
-  `\n` +
-  `<b>👥 Group:</b> ${mod}` +
-  `\n` +
-  `<b>🚦 Status:</b> ${
-    isAdmin ? "Yup... He is an admin." : "Nope... He is a civilian"
-  }`;
+export const message = (error: boolean, count?: number) =>
+  error
+    ? `You don't enough privileges to see servers stats`
+    : `Groups: ${count}`;
 
 export const keyboard = Markup.inlineKeyboard([
   [Markup.urlButton(`Other Services Health`, `https://maid.uz/status/`)],
