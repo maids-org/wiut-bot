d67637d443780359b1ee3343d3acdf742e5a484d
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-15 18:57:45 +0500

    Update resource.ts

--- src/delta/server/resource.ts
+++ src/delta/server/resource.ts
@@ -2,8 +2,8 @@ import { Markup } from "telegraf";
 
 export const message = (error: boolean, count?: number) =>
   error
-    ? `You don't enough privileges to see servers stats`
-    : `Groups: ${count}`;
+    ? `You don't <b>enough privileges</b> to see servers stats`
+    : `<b>Groups:</b> <code>${count}</code>`;
 
 export const keyboard = Markup.inlineKeyboard([
   [Markup.urlButton(`Other Services Health`, `https://maid.uz/status/`)],
