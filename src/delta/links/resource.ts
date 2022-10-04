b1631722049150bdbd72588f506e77845734cc7d
UwUssimo Robinson <uwussimo@icloud.com>
2022-10-04 13:31:44 +0500

    Update resource.ts

--- src/delta/links/resource.ts
+++ src/delta/links/resource.ts
@@ -5,15 +5,14 @@ export const message = `<b>Here are 70+ Family BIS groups & channels:</b>`;
 
 export const keyboard = async (): Promise<InlineKeyboardMarkup> => {
   return Markup.inlineKeyboard([
-    [Markup.urlButton(`Announcement Channel`, `https://t.me/SeventyPlus`)],
+    [Markup.urlButton(`Announcement Channel`, `https://t.me/madmaids`)],
+    // [Markup.urlButton(`General Chat Group (4BIS)`, `https://t.me/BIS_24`)],
     [
       Markup.urlButton(
-        `General Chat Group (5BIS)`,
-        `https://t.me/joinchat/Vao3J-FcSBf5Bo2B`
+        `Mad Maids General Community`,
+        `https://t.me/madmaids_wiut`
       ),
     ],
-    [Markup.urlButton(`General Chat Group (4BIS)`, `https://t.me/BIS_24`)],
-    [Markup.urlButton(`Mad Maids General Community`, `https://t.me/madmaids`)],
     [Markup.urlButton(`Anime focused chat group`, `https://t.me/wiutanime`)],
   ]);
 };
