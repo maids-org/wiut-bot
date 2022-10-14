e49f10628ed7eaf0eaea3fcb9fc979d47e060429
UwUssimo Robinson <uwussimo@icloud.com>
2022-10-14 10:20:50 +0500

    Update command.ts

--- src/delta/setup/command.ts
+++ src/delta/setup/command.ts
@@ -4,15 +4,13 @@ import * as resource from "./resource";
 import { TelegrafContext } from "@type/telegraf";
 
 composer.command(`setup`, async (ctx: TelegrafContext) => {
-  if (!(await dungeon.getAllID()).map((id) => id.id).includes(ctx.chat.id)) {
-    return await ctx.replyWithHTML(resource.message.notRegistered);
-  }
-
   if (ctx.chat.type === "private" || ctx.chat.type === "channel") {
     return await ctx.replyWithHTML(resource.message.noPrivate);
   }
 
-  console.log(ctx.chat.type === "private" || ctx.chat.type === "channel");
+  if (!(await dungeon.getAllID()).map((id) => id.id).includes(ctx.chat.id)) {
+    return await ctx.replyWithHTML(resource.message.notRegistered);
+  }
 
   if (!(await resource.isUserAdmin(ctx, ctx.from.id))) {
     return await ctx.replyWithHTML(resource.message.notAdmin);
