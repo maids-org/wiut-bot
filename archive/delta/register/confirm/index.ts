400c127b00a7a8f6848686259fd9cc1d8fa3f097
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-03 10:53:21 +0500

    removing ported commands

--- archive/delta/register/confirm/index.ts
+++
@@ -1,7 +0,0 @@
-import * as resource from "./resource";
-
-import "./prompt";
-import "./accept";
-import "./reject";
-
-export default resource;
--- archive/delta/register/index.ts
+++
@@ -1,7 +0,0 @@
-import * as resource from "./resource";
-
-import "./action";
-import "./command";
-import "./confirm";
-
-export default resource;
