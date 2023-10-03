e444051b85efe5a77646053b46240ec3fb5015da
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-03 19:45:52 +0500

    data fetcher seperated

--- update-data.sh
+++ update-data.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
 #
-# This script is used to update the data git submodule
+# This script is used to update the data by downloading
+# the latest version from the data repository
 #
 
 # Set working directory to the script's directory
 cd "$(dirname "$0")" || exit
 
+# If the data data exists, remove it
+if [ -d "data" ]; then
+    echo "Removing data directory..."
+    rm -rf data
+fi
+
 # Update data git submodule
 echo "Updating data git submodule..."
-git submodule update --init --recursive --remote
+
+# Download the latest version of the data repository
+wget https://github.com/mad-maids/wiut-data/archive/refs/heads/main.zip
+
+# Unzip the downloaded file
+unzip main.zip
+
+# Rename the unzipped folder to data
+mv wiut-data-main data
+
+# Clean up
+rm -rf main.zip
+rm -rf ./data/README.md
+rm -rf ./data/LICENSE
+rm -rf ./data/.github
