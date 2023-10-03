99fba4bb94fedeb0f64e55fa481481cd14eaab5a
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-10-03 19:23:25 +0500

    cronjob + docker build automation

--- update-data.sh
+++ update-data.sh
@@ -4,12 +4,6 @@
 # This script is used to update the data git submodule
 #
 
-# Check if folder "data" exists
-if [ ! -d "data" ]; then
-    echo "Folder 'data' does not exist. Fetching data git submodule..."
-    git submodule update --init --recursive --remote
-fi
-
 # Update data git submodule
 echo "Updating data git submodule..."
 git submodule update --init --recursive --remote
